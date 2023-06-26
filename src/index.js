15af551e9e8f9ad91fc64ef270832ffc04dcd84a
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-06-26 10:13:04 +0300

    New_Fix2 after checking

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import ReactDOM from 'react-dom/client';
 import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
-
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goit-react-hw-08-phonebook">
@@ -17,5 +15,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
 );
